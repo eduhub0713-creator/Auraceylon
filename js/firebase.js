c39b6a5e41904e67b7d0041809b4aa8c956fb497
eduhub0713-creator <eduhub0713@gmail.com>
2026-04-15 17:21:24 +0530

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+// Firebase App (core)
+const firebaseConfig = {
+  apiKey: "AIzaSyBw9R1fDYJLSarT_UZZSyJb3yEJl3BE20M",
+  authDomain: "auraceylon-e5b64.firebaseapp.com",
+  projectId: "auraceylon-e5b64",
+  storageBucket: "auraceylon-e5b64.firebasestorage.app",
+  messagingSenderId: "558409863488",
+  appId: "1:558409863488:web:f4167e881226e1b852aaa5",
+  measurementId: "G-5L1RQQS8NF"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
