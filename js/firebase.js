a8b075222859038f37dcc105a95ef5ec20a70499
eduhub0713-creator <eduhub0713@gmail.com>
2026-04-24 06:00:38 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,4 +1,4 @@
-// Firebase App (core)
+// Firebase App Config
 const firebaseConfig = {
   apiKey: "AIzaSyBw9R1fDYJLSarT_UZZSyJb3yEJl3BE20M",
   authDomain: "auraceylon-e5b64.firebaseapp.com",
@@ -9,5 +9,11 @@ const firebaseConfig = {
   measurementId: "G-5L1RQQS8NF"
 };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// Initialize Firebase only once
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+// Global Firebase services
+const auth = firebase.auth();
+const db = firebase.firestore();
