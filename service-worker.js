3589ce0c021f2ac2ab77cd6956bd70a2558c3671
eduhub0713-creator <eduhub0713@gmail.com>
2026-04-27 13:02:35 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,88 +1,112 @@
-const CACHE_NAME = "auraceylon-v13";
+const CACHE_NAME = "auraceylon-v14";
 
 const APP_SHELL = [
-  "./",
-  "./index.html",
-  "./products.html",
-  "./product-details.html",
-  "./cart.html",
-  "./checkout.html",
-  "./about.html",
-  "./contact.html",
-  "./offline.html",
-  "./manifest.json",
+  "/",
+  "/index.html",
+  "/products.html",
+  "/product-details.html",
+  "/cart.html",
+  "/checkout.html",
+  "/orders.html",
+  "/profile.html",
+  "/login.html",
+  "/signup.html",
+  "/forgot-password.html",
+  "/about.html",
+  "/contact.html",
+  "/offline.html",
+  "/manifest.json",
 
-  "./css/base.css",
-  "./css/home.css",
-  "./css/products.css",
-  "./css/cart.css",
-  "./css/responsive.css",
-  "./css/offline.css",
+  "/css/base.css",
+  "/css/home.css",
+  "/css/products.css",
+  "/css/cart.css",
+  "/css/responsive.css",
+  "/css/offline.css",
 
-  "./js/main.js",
-  "./js/products.js",
-  "./js/cart.js",
-  "./js/checkout.js",
-  "./js/ui.js",
-  "./js/pwa.js",
+  "/js/firebase.js",
+  "/js/main.js",
+  "/js/products.js",
+  "/js/cart.js",
+  "/js/checkout.js",
+  "/js/ui.js",
+  "/js/pwa.js",
 
-  "./assets/logo.jpg",
-  "./assets/banner.jpg",
-  "./assets/icon-192.png",
-  "./assets/icon-512.png"
+  "/assets/logo.jpg",
+  "/assets/banner.jpg",
+  "/assets/icon-192.png",
+  "/assets/icon-512.png"
 ];
 
-// Install
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(APP_SHELL);
     })
   );
+
   self.skipWaiting();
 });
 
-// Activate
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
+    caches.keys().then((keys) => {
+      return Promise.all(
         keys.map((key) => {
           if (key !== CACHE_NAME) {
             return caches.delete(key);
           }
         })
-      )
-    )
+      );
+    })
   );
+
   self.clients.claim();
 });
 
-// Fetch
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
 
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
+  const requestUrl = new URL(event.request.url);
 
-      return fetch(event.request)
-        .then((networkResponse) => {
-          const responseClone = networkResponse.clone();
+  if (
+    requestUrl.hostname.includes("firebase") ||
+    requestUrl.hostname.includes("googleapis") ||
+    requestUrl.hostname.includes("gstatic")
+  ) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const responseClone = response.clone();
 
           caches.open(CACHE_NAME).then((cache) => {
             cache.put(event.request, responseClone);
           });
 
-          return networkResponse;
+          return response;
         })
-        .catch(() => {
-          if (event.request.mode === "navigate") {
-            return caches.match("./offline.html");
-          }
+        .catch(() => caches.match("/offline.html"))
+    );
+
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request).then((networkResponse) => {
+        const responseClone = networkResponse.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
         });
+
+        return networkResponse;
+      });
     })
   );
 });
